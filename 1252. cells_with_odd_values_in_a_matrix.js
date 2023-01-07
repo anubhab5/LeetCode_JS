a47f13538a83a5184ea952f6f25edebd085a4363
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2023-01-08 00:42:27 +0530

    1252. Efficient Soln

--- 1252. cells_with_odd_values_in_a_matrix.js
+++ 1252. cells_with_odd_values_in_a_matrix.js
@@ -93,3 +93,22 @@ var oddCells = function (m, n, indices) {
     }
     return oddCount;
 };
+
+/**
+ * Efficient Soln
+ */
+
+var oddCells = function (m, n, indices) {
+    const rows = new Array(m).fill(0);
+    const cols = new Array(n).fill(0);
+
+    for (let i = 0; i < indices.length; i++) {
+        const [r, c] = indices[i];
+        rows[r] ^= 1;
+        cols[c] ^= 1;
+    }
+    const rowOdd = rows.filter(r => r).length;
+    const colOdd = cols.filter(c => c).length;
+
+    return (m - rowOdd) * colOdd + (n - colOdd) * rowOdd;
+};
