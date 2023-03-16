d8e8cc4684fe2abbfe6212885cf30c3569d2fac6
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2023-03-16 22:50:11 +0530

    26 remove duplicate - re

--- 26_removeDuplicateFromSortedArray.js
+++ 26_removeDuplicateFromSortedArray.js
@@ -15,3 +15,20 @@ var removeDuplicates = function (nums) {
 };
 const arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 console.log(removeDuplicates(arr))
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let c = 0 ;
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i+1]) {
+            nums[i] = Infinity;
+            c++;
+        }
+    }
+    nums.sort((a, b) => a-b);
+    nums.length = nums.length - c;
+    
+};
